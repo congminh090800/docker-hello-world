eb89b67ddd8c8fe1ee82151034a70cf50530583a
congminh090800 <congminh090800@gmail.com>
2022-09-23 15:41:28 +0700

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send(`Hello from app ${process.env.APP_ID}, test`);
+  res.send(`Hello from app ${process.env.APP_ID}`);
 });
 
 app.listen(port, () => {
